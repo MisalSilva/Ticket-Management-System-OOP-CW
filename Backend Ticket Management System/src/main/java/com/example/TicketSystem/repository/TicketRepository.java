241f7e84236ff521c7f1601fcb8abb770603a031
MisalSilva <misal.20230183@iit.ac.lk>
2024-12-10 14:20:01 +0530

    Update: handling database operations for Ticket Entity

---
+++ Backend Ticket Management System/src/main/java/com/example/TicketSystem/repository/TicketRepository.java
@@ -0,0 +1,9 @@
+package com.example.TicketSystem.repository;
+
+import com.example.TicketSystem.model.Ticket;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TicketRepository extends JpaRepository<Ticket, Integer> {
+}
